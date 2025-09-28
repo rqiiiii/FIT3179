baed7e7630effc2177f61dc3a3776a33b73d7aa5
rgan0020 <rgan0020@student.monash.edu>
2025-09-28 16:06:30 +0800

    organising with folder

---
+++ HW9/js/vega_lite_vis.js
@@ -0,0 +1,4 @@
+var spec2 = "js/malaysia_accidents.json";
+vegaEmbed('#vis', spec2).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);

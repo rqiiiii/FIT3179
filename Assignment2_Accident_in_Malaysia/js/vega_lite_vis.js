04fe7da7b34a6b32772afa6f0c64f07c136d0358
rgan0020 <rgan0020@student.monash.edu>
2025-10-20 13:51:49 +0800

    added world map to show death rate by road accident

--- Assignment2_Accident_in_Malaysia/js/vega_lite_vis.js
+++ Assignment2_Accident_in_Malaysia/js/vega_lite_vis.js
@@ -1,5 +1,5 @@
 var spec1 = "js/accident_injuries_roaduser.json";
-vegaEmbed('#halfdonut', spec1, {
+vegaEmbed('#donut', spec1, {
   actions: false,   // hide "Open in Vega Editor" buttons
   renderer: 'canvas'
 }).catch(console.error);
@@ -10,7 +10,7 @@ vegaEmbed('#choroplethmap', spec2).then(function(result) {
 }).catch(console.error);
 
 var spec3 = "js/accident_severity.json";
-vegaEmbed('#stackbar', spec3, {
+vegaEmbed('#bump', spec3, {
   actions: false,   // hide "Open in Vega Editor" buttons
   renderer: 'canvas'
 }).catch(console.error);
@@ -19,4 +19,9 @@ var spec4 = "js/accident_condition.json";
 vegaEmbed('#areastack', spec4, {
   actions: false,   // hide "Open in Vega Editor" buttons
   renderer: 'canvas'
+}).catch(console.error);
+
+var spec5 = "js/world_accident_death_rate.json";
+vegaEmbed('#worldmap', spec5).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);

d16144241491fd2f37a5e274838cf0b0957ed7b0
rgan0020 <rgan0020@student.monash.edu>
2025-10-21 20:19:59 +0800

    Edited ToolTips and moved css to another file

--- Assignment2_Accident_in_Malaysia/js/vega_lite_vis.js
+++ Assignment2_Accident_in_Malaysia/js/vega_lite_vis.js
@@ -1,7 +1,10 @@
 var spec5 = "js/world_accident_death_rate.json";
-vegaEmbed('#worldmap', spec5).then(function(result) {
-    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
-}).catch(console.error);
+vegaEmbed('#worldmap', spec5, { actions: false })
+  .then(function(result) {
+    // You can access result.view here if needed
+  })
+  .catch(console.error);
+
 
 
 var spec1 = "js/accident_injuries_roaduser.json";
@@ -11,13 +14,14 @@ vegaEmbed('#donut', spec1, {
 }).catch(console.error);
 
 var spec2 = "js/total_accidents.json";
-vegaEmbed('#choroplethmap', spec2).then(function(result) {
+vegaEmbed('#choroplethmap', spec2, { actions: false })
+.then(function(result) {
     // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
 
 var spec3 = "js/accident_severity.json";
-vegaEmbed('#bump', spec3, {
-  title: null
+vegaEmbed('#bump', spec3, { actions: false })
+.then(function(result) {
 }).catch(console.error);
 
 var spec4 = "js/accident_condition.json";
